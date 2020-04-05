2b51d3d3b260f7759a9030df5bf3a1282e318168
Maxime Bouthors <maxbouthors@gmail.com>
2020-04-06 00:22:51 +0200

    file moved

--- raw_audio_gender_classification/neat/rnn.py
+++ raw_audio_gender_classification/neat/rnn.py
@@ -22,6 +22,8 @@ from raw_audio_gender_classification.utils import whiten
 
 os.environ["PATH"] += os.pathsep + "C:\\Program Files (x86)\\graphviz\\bin"
 
+torch.cuda.set_device(0)
+
 training_set = ['train-clean-100', 'train-clean-360']
 validation_set = 'dev-clean'
 n_seconds = 3
@@ -30,16 +32,20 @@ batch_size = 15
 
 
 class BasicRNN(nn.Module):
-    def __init__(self, n_inputs, n_neurons):
+    def __init__(self, n_inputs, n_neurons, device="cuda"):
         super(BasicRNN, self).__init__()
         self.n_neurons = n_neurons
+        self.device = torch.device(device)
         self.rnn = nn.RNNCell(n_inputs, n_neurons)
-        self.hx = torch.randn(batch_size, n_neurons)  # initialize hidden state
+        self.hx = torch.randn(batch_size, n_neurons, device=device)  # initialize hidden state
         self.fc = nn.Linear(n_neurons, 1)
+        if device == "cuda":
+            self.rnn.cuda()
+            self.fc.cuda()
 
     def init_hidden(self, ):
         # (num_layers, batch_size, n_neurons)
-        return torch.zeros(batch_size, self.n_neurons)
+        return torch.zeros(batch_size, self.n_neurons, device=self.device)
 
     def forward(self, x):
 
@@ -61,6 +67,10 @@ class BasicRNN(nn.Module):
         out = torch.sigmoid(self.fc(out))
 
         return out
+
+    def cuda(self, **kwargs):
+        super(BasicRNN, self).cuda(**kwargs)
+        self.rnn.cuda()
 
 
 def preprocessor(batch, batchsize=batch_size):
@@ -85,18 +95,22 @@ if __name__ == '__main__':
 
     trainloader, testloader = load_data()
 
-    rnn = BasicRNN(1, 40)
+    rnn = BasicRNN(1, 40, device="cpu")
+
+    # rnn.cuda()
 
     criterion = nn.MSELoss()
     optimizer = optim.Adam(rnn.parameters(), lr=0.001)
 
-    writer = SummaryWriter('runs/experiment_rnn')
+    writer = SummaryWriter('../../runs/experiment_rnn')
+
 
     def get_accuracy(logit, target):
         """ Obtain accuracy for training round """
         corrects = (torch.abs(logit - target) < 0.5).sum()
         accuracy = 100.0 * corrects / batch_size
         return accuracy.item()
+
 
     for epoch in range(1):  # loop over the dataset multiple times
         train_running_loss = 0.0
@@ -113,8 +127,8 @@ if __name__ == '__main__':
 
             # get the inputs
             inputs, labels = data
-            labels = labels.float()
-            inputs = inputs.view(-1, 48000)
+            labels = labels.float()  # .cuda()
+            inputs = inputs.view(-1, 48000)  # .cuda()
 
             # forward + backward + optimize
             outputs = rnn(inputs).view(batch_size)
@@ -129,4 +143,4 @@ if __name__ == '__main__':
 
         rnn.eval()
         print('Epoch:  %d | Loss: %.4f | Train Accuracy: %.2f'
-              % (epoch, train_running_loss/len(trainloader), train_acc/len(trainloader)))
+              % (epoch, train_running_loss / len(trainloader), train_acc / len(trainloader)))
