fd938c7cef225805f361a83fdf1d71619b7e1d6e
Maxime Bouthors <maxbouthors@gmail.com>
2020-06-11 21:15:21 +0200

    optuna with MNIST

--- hyperneat/MNIST_HyperNEAT.py
+++ hyperneat/MNIST_HyperNEAT.py
@@ -11,6 +11,8 @@ import matplotlib.pyplot as plt
 from utils import smooth
 import time
 import numpy as np
+import optuna
+import pandas as pd
 
 device = torch.device("cuda")
 
@@ -22,12 +24,13 @@ class SampleIterator(Iterator):
         self.batch_size = batch_size
         self.i = 0
         self.shuffle = np.random.permutation(len(self.data))
-        # self.shuffle = np.arange(len(self.data))
+        self.shuffle = np.arange(len(self.data))
 
     def __len__(self):
         return len(self.data) // self.batch_size
 
     def __next__(self):
+        self.i = 0
         X = torch.empty(self.batch_size, 1, 28, 28)
         y = torch.empty(self.batch_size)
         for b in range(self.batch_size):
@@ -68,11 +71,12 @@ class MNISTEvaluator:
         batch = next(self.data_iter)
         for _, genome in genomes:
             a = time.time()
-            genome.fitness = self.eval_function(genome, config, batch, acc=False)
+            genome.fitness = 100 * self.eval_function(genome, config, batch, acc=False)
             b = time.time()
 
     def accuracy(self, genome, config):
-        return self.eval_function(genome, config, next(self.data_iter), acc=True)
+        batch = next(self.data_iter)
+        return self.eval_function(genome, config, batch, acc=True)
 
 
 class AccuracyReporter(neat.reporting.BaseReporter):
@@ -85,7 +89,7 @@ class AccuracyReporter(neat.reporting.BaseReporter):
         self.accuracy.append(self.evaluator.accuracy(best_genome, config))
 
 
-def load_MNIST(train=True):
+def load_MNIST(train=True, batch_size=10):
     data = torchvision.datasets.MNIST('/TESTS/files/', train=train, download=True,
                                       transform=torchvision.transforms.Compose([
                                           torchvision.transforms.ToTensor(),
@@ -93,7 +97,7 @@ def load_MNIST(train=True):
                                               (0.1307,), (0.3081,))
                                       ]))
 
-    data_iter = SampleIterator(data, batch_size=100)
+    data_iter = SampleIterator(data, batch_size=batch_size)
 
     return data_iter
 
@@ -110,13 +114,15 @@ def eval_genome(g, c, batch, acc=False):
 
     cnn = MNISTCNNClassical(cppn, device).to(device)
 
-    # print("offset =", cnn.l1.offset)
+    # print("offset =", cnn.l1.offset)zqsdqszdqqzzzzz
 
     out = cnn(batch[0])
 
     if acc:
         # torch.nn.functional.one_hot(out, num_classes=10)
         y_pred = out.argmax(dim=1)
+        print("true: ", batch[1].long())
+        print("pred: ", y_pred)
         return ((y_pred == batch[1].long()).float().sum() / len(y_pred)).item()
 
     loss = torch.nn.CrossEntropyLoss()(out, batch[1].long())
@@ -124,7 +130,13 @@ def eval_genome(g, c, batch, acc=False):
     return 1 / (1 + loss.detach().item())
 
 
-def run(c, num_gen=100):
+def run(c, num_gen=100, params=None, display=False):
+
+    if params is None:
+        params = {}
+    for p in params:
+        setattr(c.genome_config, p, params[p])
+
 
     pop = neat.Population(c)
 
@@ -145,17 +157,19 @@ def run(c, num_gen=100):
 
     print("execution time:", end - start)
 
-    visualize.draw_net(c, winner, True, filename="graph_hyperneat_test")
-    visualize.plot_stats(stats, ylog=False, view=True, filename="stats_hyperneat_test")
-    plt.figure()
-    plt.plot(smooth(acc_reporter.accuracy, momentum=0.995))
-    plt.show()
-    visualize.plot_species(stats, view=True, filename="species_hyperneat_test1")
+    if display:
+        visualize.draw_net(c, winner, True, filename="graph_hyperneat_test")
+        visualize.plot_stats(stats, ylog=False, view=True, filename="stats_hyperneat_test")
+        plt.figure()
+        plt.plot(smooth(acc_reporter.accuracy, momentum=0.995))
+        plt.show()
+        visualize.plot_species(stats, view=True, filename="species_hyperneat_test1")
 
     return winner
 
 
-if __name__ == "__main__":
+def objective(trial):
+
     local_dir = os.path.dirname(__file__)
 
     config_path = os.path.join(local_dir, 'mnist_neat.cfg')
@@ -163,4 +177,52 @@ if __name__ == "__main__":
                          neat.DefaultSpeciesSet, neat.DefaultStagnation,
                          config_path)
 
-    run(config, num_gen=1000)
+    conn_add_prob = trial.suggest_uniform("conn_add_prob", 0.01, 0.2)  # 0.01, 0.2
+    # conn_delete_prob = trial.suggest_uniform("conn_delete_prob", 0.005, 0.1)  # 0.005, 0.1
+    #
+    # node_add_prob = trial.suggest_uniform("node_add_prob", 0.001, 0.2)  # 0.001, 0.2
+    # node_delete_prob = trial.suggest_uniform("node_delete_prob", 0.001, 0.1)  # 0.001, 0.1
+    #
+    # bias_mutate_power = trial.suggest_uniform("bias_mutate_power", 0.01, 1.)  # 0.01, 1.
+    # weight_mutate_power = trial.suggest_uniform("weight_mutate_power", 0.01, 1.)  # 0.01, 1.
+    #
+    # compatibility_disjoint_coefficient = trial.suggest_uniform("compatibility_disjoint_coefficient", 0.8, 1.2)  # 0.8, 1.2
+    # compatibility_weight_coefficient = trial.suggest_uniform("compatibility_weight_coefficient", 0.3, 0.7)  # 0.3, 0.7
+
+    conn_add_prob = 0.01
+    conn_delete_prob = 0.03
+    node_add_prob = 0.066
+    node_delete_prob = 0.05
+    bias_mutate_power = 0.6
+    weight_mutate_power = 0.5
+    compatibility_disjoint_coefficient = 1.15
+    compatibility_weight_coefficient = 0.5
+
+    params = {"conn_add_prob": conn_add_prob,
+              "conn_delete_prob": conn_delete_prob,
+              "node_add_prob": node_add_prob,
+              "node_delete_prob": node_delete_prob,
+              "bias_mutate_power": bias_mutate_power,
+              "weight_mutate_power": weight_mutate_power,
+              "compatibility_disjoint_coefficient": compatibility_disjoint_coefficient,
+              "compatibility_weight_coefficient": compatibility_weight_coefficient
+              }
+
+    winner = run(config, num_gen=2500, params=params)
+
+    data_iter = load_MNIST(train=True, batch_size=10)
+
+    batch = next(data_iter)
+    fitness = eval_genome(winner, config, batch, acc=False)
+
+    return fitness
+
+
+if __name__ == "__main__":
+
+    study = optuna.create_study(direction="maximize")
+    study.optimize(objective, n_trials=10)
+
+    df = study.trials_dataframe()
+
+    df.to_csv("best_params_runs.csv", index=False)
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 
 def smooth(values, momentum=0.99):
 
@@ -12,3 +14,13 @@ def smooth(values, momentum=0.99):
         w = 1 + mu * w
 
     return smoothed
+
+
+def smooth2(values, window=10):
+
+    vals = np.full(values.size + window - 1, values[0])
+    vals[window-1:] = values
+    
+    w = np.ones(window) / window
+
+    return np.convolve(vals, w, mode='valid')
