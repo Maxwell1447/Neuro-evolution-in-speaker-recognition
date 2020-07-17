25ed785afbe2dc3379cb0a2134fe3f6c0375f6fa
Maxwell1447 <maxbouthors@gmail.com>
2020-07-17 15:53:15 +0200

    math typo corrected

--- neat_local/scheduler.py
+++ neat_local/scheduler.py
@@ -58,7 +58,7 @@ class SineScheduler(BaseReporter):
         print("--Scheduler Values--")
         for key in self.final_values:
             b, a = self.final_values[key], self.init_values[key]
-            value = (a + b) / 2 + (a - b) * 2 * cos(2 * pi * self.gen / self.period)
+            value = (a + b) / 2 + (a - b) / 2 * cos(2 * pi * self.gen / self.period)
             print(key, value)
 
             setattr(conf.genome_config, key, value)
