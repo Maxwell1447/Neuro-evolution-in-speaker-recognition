f76bc662932732033f4f3b47546243ba41713826
Arnaud Barral <zephir940@gmail.com>
2020-08-06 23:38:51 +0200

    Correction of eoc and added aggregator with different resolutions

--- anti_spoofing/main_train_short_aggregator.py
+++ anti_spoofing/main_train_short_aggregator.py
@@ -5,16 +5,70 @@ import os
 import pickle
 
 from anti_spoofing.data_utils import ASVDataset
-from anti_spoofing.data_utils_short import ASVDatasetshort
 from anti_spoofing.utils_ASV import whiten, gate_mfcc
 from anti_spoofing.metrics_utils import rocch2eer, rocch
+
+
+def compute_eer(target_scores, non_target_scores):
+    target_scores = np.array(target_scores)
+    non_target_scores = np.array(non_target_scores)
+    pmiss, pfa = rocch(target_scores, non_target_scores)
+    eer = rocch2eer(pmiss, pfa)
+    return eer
+
+
+def evaluate_different_window(net, data_loader):
+    """
+    compute the eer equal error rate
+    :param net: list of networks
+    :param data_loader: list of dataset, contains audio files or extracted features in a numpy array format
+    :return eer
+    """
+    nb_net = len(net)
+    target_scores_sum = []
+    non_target_scores_sum = []
+    target_scores_prod = []
+    non_target_scores_prod = []
+    target_scores_min = []
+    non_target_scores_min = []
+    target_scores_max = []
+    non_target_scores_max = []
+    target_scores_median = []
+    non_target_scores_median = []
+    for index_data in tqdm(range(len(data_loader[0]))):
+        xo = np.zeros(nb_net)
+        for i in range(nb_net):
+            net[i].reset()
+            sample_input, output = data_loader[i][index_data][0], data_loader[i][index_data][1]
+            sample_input = whiten(sample_input)
+            xo[i] = gate_mfcc(net[i], sample_input)
+        if output == 1:
+            target_scores_sum.append(xo.mean())
+            target_scores_prod.append(xo.prod())
+            target_scores_min.append(xo.min())
+            target_scores_max.append(xo.max())
+            target_scores_median.append(np.median(xo))
+        else:
+            non_target_scores_sum.append(xo.mean())
+            non_target_scores_prod.append(xo.prod())
+            non_target_scores_min.append(xo.min())
+            non_target_scores_max.append(xo.max())
+            non_target_scores_median.append(np.median(xo))
+
+    eer_sum = compute_eer(target_scores_sum, non_target_scores_sum)
+    eer_prod = compute_eer(target_scores_prod, non_target_scores_prod)
+    eer_min = compute_eer(target_scores_min, non_target_scores_min)
+    eer_max = compute_eer(target_scores_max, non_target_scores_max)
+    eer_median = compute_eer(target_scores_median, non_target_scores_median)
+
+    return eer_sum, eer_prod, eer_min, eer_max, eer_median
 
 
 def evaluate(net, data_loader):
     """
     compute the eer equal error rate
-    :param net: network
-    :param data_loader: test dataset, contains audio files in a numpy array format
+    :param net: list of networks
+    :param data_loader: dataset, contains audio files or extracted features in a numpy array format
     :return eer
     """
     nb_net = len(net)
@@ -49,35 +103,11 @@ def evaluate(net, data_loader):
             non_target_scores_max.append(xo.max())
             non_target_scores_median.append(np.median(xo))
 
-    target_scores_sum = np.array(target_scores_sum)
-    non_target_scores_sum = np.array(non_target_scores_sum)
-
-    target_scores_prod = np.array(target_scores_prod)
-    non_target_scores_prod = np.array(non_target_scores_prod)
-
-    target_scores_min = np.array(target_scores_min)
-    non_target_scores_min = np.array(non_target_scores_min)
-
-    target_scores_max = np.array(target_scores_max)
-    non_target_scores_max = np.array(non_target_scores_max)
-
-    target_scores_median = np.array(target_scores_median)
-    non_target_scores_median = np.array(non_target_scores_median)
-
-    pmiss_sum, pfa_sum = rocch(target_scores_sum, non_target_scores_sum)
-    eer_sum = rocch2eer(pmiss_sum, pfa_sum)
-
-    pmiss_prod, pfa_prod = rocch(target_scores_prod, non_target_scores_prod)
-    eer_prod = rocch2eer(pmiss_prod, pfa_prod)
-
-    pmiss_min, pfa_min = rocch(target_scores_min, non_target_scores_min)
-    eer_min = rocch2eer(pmiss_min, pfa_min)
-
-    pmiss_max, pfa_max = rocch(target_scores_max, non_target_scores_max)
-    eer_max = rocch2eer(pmiss_max, pfa_max)
-
-    pmiss_median, pfa_median = rocch(target_scores_median, non_target_scores_median)
-    eer_median = rocch2eer(pmiss_median, pfa_median)
+    eer_sum = compute_eer(target_scores_sum, non_target_scores_sum)
+    eer_prod = compute_eer(target_scores_prod, non_target_scores_prod)
+    eer_min = compute_eer(target_scores_min, non_target_scores_min)
+    eer_max = compute_eer(target_scores_max, non_target_scores_max)
+    eer_median = compute_eer(target_scores_median, non_target_scores_median)
 
     return eer_sum, eer_prod, eer_min, eer_max, eer_median
 
@@ -93,23 +123,44 @@ if __name__ == '__main__':
                          neat.DefaultSpeciesSet, neat.DefaultStagnation,
                          config_path)
 
-    trainset = ASVDataset(is_train=True, nb_samples=80000, do_mfcc=True)
-    devset = ASVDataset(is_train=False, is_eval=False, nb_samples=80000, do_mfcc=True)
-    testset = ASVDataset(is_train=False, is_eval=True, nb_samples=80000, do_mfcc=True)
 
-    net_1 = pickle.load(open('best_genome_eoc_batch_128_c3', 'rb'))
-    net_2 = pickle.load(open('best_genome_eoc_batch_128_nfft_1024', 'rb'))
-    net_3 = pickle.load(open('best_genome_eoc_batch_128_nfft_512', 'rb'))
+    net_1_0 = pickle.load(open('best_genome_eoc_class_1', 'rb'))
+    net_1_1 = pickle.load(open('best_genome_eoc_class_1_test', 'rb'))
+    net_1_2 = pickle.load(open('best_genome_eoc_class_1_test0', 'rb'))
+    net_2 = pickle.load(open('best_genome_eoc_class_2_test', 'rb'))
+    net_2_0 = pickle.load(open('best_genome_eoc_class_2', 'rb'))
+    net_3 = pickle.load(open('best_genome_eoc_class_3', 'rb'))
+    net_4 = pickle.load(open('best_genome_eoc_class_4', 'rb'))
+    net_5 = pickle.load(open('best_genome_eoc_class_5', 'rb'))
+    net_6 = pickle.load(open('best_genome_eoc_class_6_test', 'rb'))
 
-    net = [net_1, net_2, net_3]
+    aggregate_net = []
+    train_eer = evaluate(aggregate_net, trainset)
+    dev_eer = evaluate(aggregate_net, devset)
+    eer = evaluate(aggregate_net, testset)
+
+
+
+    net_best = pickle.load(open('best_genome_eoc_batch_128_c3', 'rb'))
+    net_ = pickle.load(open('best_genome_eoc_batch_128_nfft_512', 'rb'))
+    net_b = pickle.load(open('best_genome_eoc_batch_128_nfft_1024', 'rb'))
+
+    net = [net_best, net_, net_b]
 
     aggregate_net = []
     for i in range(len(net)):
         aggregate_net.append(neat.nn.RecurrentNetwork.create(net[i], config))
 
-    train_eer = evaluate(aggregate_net, trainset)
-    dev_eer = evaluate(aggregate_net, devset)
-    eer = evaluate(aggregate_net, testset)
+
+    eval_512 = pickle.load(open('dataset_eval_mfcc_512', 'rb'))
+    eval_1024 = pickle.load(open('dataset_eval_mfcc_1024', 'rb'))
+    eval_2048 = pickle.load(open('dataset_eval_mfcc_2048', 'rb'))
+
+    eval_dataset = [eval_2048, eval_512, eval_1024]
+
+    eer = evaluate_different_window(aggregate_net, eval_dataset)
+
+
 
     print("\n")
     print("**** equal error rate train = {}  ****".format(train_eer))
