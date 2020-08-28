61306e730a1b13f9d13d026bfc4ee0786e3e62b1
Maxime Bouthors <maxbouthors@gmail.com>
2020-08-28 13:47:53 +0200

    wip

--- anti_spoofing/main_train.py
+++ anti_spoofing/main_train.py
@@ -191,7 +191,7 @@ if __name__ == '__main__':
     # here so that the script will run successfully regardless of the
     # current working directory.
     local_dir = os.path.dirname(__file__)
-    config_path = os.path.join(local_dir, 'ASV_neat_preprocessed{}.cfg'.format('_backprop' if backprop else ''))
+    config_path = os.path.join(local_dir, 'ASV_neat_preprocessed{}.cfg'.format('_backprop' if backprop else '_long'))
 
     if OPTION == "cqcc":
         train_data, devloader = load_data_cqcc(batch_size=100, num_train=10000, num_test=10000, balanced=True)
